89a13f12d3510ff8a244c77e5f9f639256805638
陳博煒 <rayologist1002@gmail.com>
2021-07-30 00:00:10 +0800

    Adding RGB and HEX conversion

--- script.js
+++ script.js
@@ -5,13 +5,17 @@ const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
 const randomBtn = document.querySelector(".btn-random");
-const copyBtn = document.querySelector(".btn-copy");
+const fullCSSCopyBtn = document.querySelector("#full-css-btn");
+const RGBCopyBtn = document.querySelector("#rgb-btn");
+const hexCopyBtn = document.querySelector("#hex-btn");
 const exchangeBtn = document.querySelector(".btn-exchange");
 
-function copyBackgroundColor(){
+
+
+function copyBackgroundColor(copyBtn, node){
     const selection = window.getSelection();
     const range = document.createRange();
-    range.selectNodeContents(fullCSS);
+    range.selectNodeContents(node);
     selection.removeAllRanges();
     selection.addRange(range);
     let res = document.execCommand("copy");
@@ -35,17 +39,29 @@ function getRandomColor(){
     return "#" + String(randomNumber).toLowerCase();
 }
 
-function setGradient(cssNode){
+function toRGB(hex){
+    let hex_value = hex.replace("#", "");
+    let r = parseInt(hex_value.slice(0, 2), 16);
+    let g = parseInt(hex_value.slice(2, 4), 16);
+    let b = parseInt(hex_value.slice(4, 6), 16);
+    return `rgb(${r}, ${g}, ${b})`;
+
+}
+
+function setGradient(color1, color2){
     bgColor = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
     body.style.background = bgColor;
-    cssNode.textContent = body.style.background + ";";
+    fullCSS.textContent = body.style.background + ";";
+    RGBText.textContent = toRGB(color1.value) + ", " + toRGB(color2.value);
+    hexText.textContent = color1.value + ", " + color2.value;
+
 }
 
 function exchangeGradient(){
     let temp = color1.value
     color1.value = color2.value
     color2.value = temp
-    setGradient(fullCSS);
+    setGradient(color1, color2);
 }
 
 function randomizeGradient(){
@@ -53,11 +69,13 @@ function randomizeGradient(){
     let rand2 = getRandomColor();
     color1.value = rand1;
     color2.value = rand2;
-    setGradient(fullCSS);
+    setGradient(color1, color2);
 }
 
 randomBtn.addEventListener('click', randomizeGradient)
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
-copyBtn.addEventListener("click", copyBackgroundColor);
+fullCSSCopyBtn.addEventListener("click", ()=>copyBackgroundColor(fullCSSCopyBtn, fullCSS));
+RGBCopyBtn.addEventListener("click", ()=>copyBackgroundColor(RGBCopyBtn, RGBText));
+hexCopyBtn.addEventListener("click", ()=>copyBackgroundColor(hexCopyBtn, hexText));
 exchangeBtn.addEventListener("click", exchangeGradient)
